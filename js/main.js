a92f6ec9c13ec3d15a8e4f57aae8667f148f9a50
Vinícius Raniery <124690508+V1niciusR@users.noreply.github.com>
2023-07-05 15:04:19 -0300

    Introdução das FAQs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-window.onscroll = function() {
-    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0 ) {
+window.onscroll = function () {
+    if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
         document.getElementById('navbar').classList.add('scrolled');
     } else {
         document.getElementById('navbar').classList.remove('scrolled');
@@ -10,3 +10,20 @@ window.onscroll = function() {
 AOS.init({
     duration: 800,
 });
+
+
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+var len = acc.length;
+for (i = 0; i < len; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
